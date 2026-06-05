f54455865b61e10f7399c616487a1c9a42ff33d6
Iqra Almani <iqra.almani@iga.in.gov>
2026-06-05 19:10:41 -0400

    Restrict file version serializer fields

--- src/propylon_document_manager/file_versions/api/serializers.py
+++ src/propylon_document_manager/file_versions/api/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
-
 from ..models import FileVersion
 
 class FileVersionSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileVersion
-        fields = "__all__"
+        fields = [
+            "id",
+            "file_name",
+            "file_url",
+            "version_number",
+            "content_hash",
+            "created_at",
+        ]
+        read_only_fields = [
+            "id",
+            "file_name",
+            "file_url",
+            "version_number",
+            "content_hash",
+            "created_at",
+        ]
